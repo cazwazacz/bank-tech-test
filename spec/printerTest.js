ae41069ffaa5137d4596ec3449ab9825955a4bf4
Allan Wazacz <cazwazacz@gmail.com>
2017-12-31 18:44:35 +0000

    Add print method which prints out statement in a table

--- spec/printerTest.js
+++ spec/printerTest.js
@@ -1,6 +1,12 @@
 describe("Printer", function() {
 
   var printer;
+  var array = [
+    {date: '10-01-2012', transactionType: 'deposit', amount: 1000, balance: 1000},
+    {date: '11-01-2012', transactionType: 'withdrawal', amount: 700, balance: 300},
+    {date: '12-01-2012', transactionType: 'withdrawal', amount: 100, balance: 200},
+    {date: '13-01-2012', transactionType: 'deposit', amount: 2000, balance: 2200},
+  ];
 
   beforeEach(function() {
     printer = new Printer();
@@ -8,6 +14,15 @@ describe("Printer", function() {
 
   it("exists", function() {
     expect(printer).toEqual(jasmine.any(Printer));
+  })
+
+  it("prints out the array in table format", function() {
+    var string = "date || credit || debit || balance\n" +
+                  "13-01-2012 || 2000.00 || || 2200.00\n" +
+                  "12-01-2012 || || 100.00 || 200.00\n" +
+                  "11-01-2012 || || 700.00 || 300.00\n" +
+                  "10-01-2012 || 1000.00 || || 1000.00";
+    expect(printer.print(array)).toEqual(string);
   })
 
 })
--- src/printer.js
+++ src/printer.js
@@ -1,1 +1,14 @@
 function Printer() {};
+
+Printer.prototype.print = function (history) {
+  var history = history.reverse();
+  var array = ["date || credit || debit || balance"];
+  history.forEach(function(transaction) {
+    if (transaction.transactionType === 'deposit') {
+      array.push(`${transaction.date} || ${(transaction.amount).toFixed(2)} || || ${(transaction.balance).toFixed(2)}`);
+    } else {
+      array.push(`${transaction.date} || || ${(transaction.amount).toFixed(2)} || ${(transaction.balance).toFixed(2)}`);
+    }
+  })
+  return array.join("\n");
+};
