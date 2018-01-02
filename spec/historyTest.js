97ebc6bbde223578d097bb11c11bde1dd4b9062e
Allan Wazacz <cazwazacz@gmail.com>
2018-01-02 10:28:21 +0000

    Add recordTransaction method to History

--- spec/historyTest.js
+++ spec/historyTest.js
@@ -8,4 +8,16 @@ describe("History", function() {
   it("has an empty array on construction", function() {
     expect(history.show()).toEqual([]);
   })
+
+  it("records history of transactions", function() {
+    history.recordTransaction('10-01-2012', 'deposit', 1000, 1000);
+    history.recordTransaction('11-01-2012', 'deposit', 2000, 3000);
+    history.recordTransaction('12-01-2012', 'withdrawal', 500, 2500);
+    var array = [
+      {date: '10-01-2012', transactionType: 'deposit', amount: 1000, balance: 1000},
+      {date: '11-01-2012', transactionType: 'deposit', amount: 2000, balance: 3000},
+      {date: '12-01-2012', transactionType: 'withdrawal', amount: 500, balance: 2500}
+    ]
+    expect(history.show()).toEqual(array);
+  })
 })
--- src/history.js
+++ src/history.js
@@ -5,3 +5,7 @@ function History() {
 History.prototype.show = function () {
   return this._history;
 };
+
+History.prototype.recordTransaction = function (date, type, amount, balance) {
+  this._history.push({date: date, transactionType: type, amount: amount, balance: balance});
+};
